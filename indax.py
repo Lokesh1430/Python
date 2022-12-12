21f478e199e1f051ae71ffc9c92cbbbbb7a2cea9
Lokesh1430 <109786176+Lokesh1430@users.noreply.github.com>
2022-12-12 20:29:26 +0530

    Add files via upload

---
+++ indax.py
@@ -0,0 +1,8 @@
+# Write a program to read n number of values in the list and display 0, 2, and 4 index values?
+
+n=int(input('Enter the total number:'))
+l=list(map(int,input('Enter the number:').split()))
+print(l)
+print(l[0])
+print(l[2])
+print(l[4])
